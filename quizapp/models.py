030b3c3b20fd8fb8daa9081983278a3b6a35358f
atheenaantonyt9689 <atheenaantony8@gmail.com>
2021-03-12 16:49:39 +0530

    models.py corrected

--- quizapp/models.py
+++ quizapp/models.py
@@ -4,15 +4,19 @@ from django.db import models
 class Quiz(models.Model):
     title = models.CharField(max_length=220)
     category = models.CharField(max_length=220)
+    def __str__(self):
+        return self.title
 
 class Question(models.Model):
+    
     question_text = models.CharField(max_length=220)
     quiz = models.ForeignKey('Quiz',on_delete=models.CASCADE)
-    correct_choice = models. ForeignKey('Choices',on_delete=models.CASCADE,) 
-
-class Choices(models.Model):
-    choice_text = models.CharField(max_length=200)
+    correct_choice = models. ForeignKey('Choice',on_delete=models.CASCADE,blank=True,null=True) 
+    def __str__(self):
+        return self.question_text
+    
+class Choice(models.Model):
+    choice_text = models.TextField()
     question_field =models.ForeignKey('Question',on_delete = models.CASCADE,)
-
-
-
+    def __str__(self):
+        return self.choice_text
